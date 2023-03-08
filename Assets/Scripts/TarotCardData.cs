cd84495213bb1bfc8b7124bcfff5d3ba5e9e1512
sol <solomon.lutze@gmail.com>
2023-03-08 11:48:17 -0500

    moving over everything from old repo

---
+++ Assets/Scripts/TarotCardData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AK;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+// Stores individual card data.
+// Create more of these to add new cards to the deck!
+
+public enum CardSuit { MajorArcana, Cups, Swords, Pentacles, Wands }
+public enum CardVideoType {Page, Knight, Queen, King, WandsPip, SwordsPip, PentaclesPip, CupsPip, MajorArcana}
+public class CardGeneratedEffects : ScriptableObject {
+    public Color color;
+}
+
+// [CreateAssetMenu(fileName = "New TarotCardData", menuName = "Tarot Card Data", order = 51)] // adds menu option to create new card data
+public class TarotCardData : ScriptableObject
+{
+    public string cardName;
+    public int order;
+    public CardSuit suit;
+    public Texture2D cardPicture;
+    public Sprite cardPicture2x;
+    [TextArea]
+    public string cardLongDescription;
+    [TextArea]
+    public string cardShortDescription;
+    public AK.Wwise.Event readingMusicEvent;
+    public int clipDuration;
+    public int thematicGroup;
+    public CardVideoType videoType;
+
+#if UNITY_EDITOR
+    // The following is a helper that adds a menu item to create an TarotCardData Asset
+    [MenuItem("Assets/Create/TarotCardData")]
+        public static void CreateTarotCardData()
+        {
+            string path = EditorUtility.SaveFilePanelInProject("Save Tarot Card", "New Tarot Card", "Asset", "Save Tarot Card", "Assets/Resources/CardData/");
+            if (path == "")
+                return;
+            AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<TarotCardData>(), path);
+        }
+    #endif
+}
---
+++ Assets/Scripts/UI/VideoCardUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class VideoCardUI : BaseUICanvas
+{
+  public LayoutElement cardDefinitionText;
+  public TextMeshProUGUI cardTitleText;
+  public TextMeshProUGUI cardDescriptionShortText;
+  private GenerativeUI generativeUI;
+
+  public void Start()
+  {
+  }
+
+
+  public void SetTextFromCardData(TarotCardData cardData)
+  {
+    cardTitleText.text = cardData.name;
+    cardDescriptionShortText.text = cardData.cardShortDescription;
+  }
+
+  public void Reset()
+  {
+    // textCanvasGroup.alpha = 0;
+    // text.maxVisibleCharacters = 0;\
+    // ReadingUtils.HideAllCharacters(text);
+  }
+}
